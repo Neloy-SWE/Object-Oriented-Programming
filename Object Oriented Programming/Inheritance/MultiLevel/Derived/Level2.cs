cbed697ebde871ae852a078a0557bf960220de93
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-01 21:58:55 +0600

    level 2 derived class added

---
+++ Object Oriented Programming/Inheritance/MultiLevel/Derived/Level2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.MultiLevel.Derived
+{
+    internal class Level2 : Level1
+    {
+        public new void CheckResponsibilities()
+        {
+            Console.WriteLine("Responsibilities:\n");
+            Console.WriteLine("Review code");
+            Console.WriteLine("Report to manager");
+            Console.WriteLine("\n\n");
+        }
+
+        public void ExtraResponsibilities()
+        {
+            Console.WriteLine("Extra responsibilities:\n");
+            Console.WriteLine("Bug fix");
+            Console.WriteLine("\n\n");
+        }
+    }
+}
