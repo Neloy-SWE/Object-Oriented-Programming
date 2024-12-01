88bc8cbb7ed2fa8c7491988d463f4d565ce3844b
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-01 21:58:10 +0600

    multi level inheritance base class added

---
+++ Object Oriented Programming/Inheritance/MultiLevel/Level.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.MultiLevel
+{
+    internal class Level
+    {
+        public void CheckResponsibilities()
+        {
+            Console.WriteLine("Call this method to check responsibilities");
+        }
+    }
+}
