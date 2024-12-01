88324cfceb0db840f8a2dfdf3f7d89b86c8a6814
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-01 12:20:21 +0600

    derived class engineer added

---
+++ Object Oriented Programming/Inheritance/Single/Derived/Engineer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.Single.Derived
+{
+    internal class Engineer : Employee
+    {
+        private int level;
+        public Engineer(int id, string name, string position, int positionId, int level) : base(id, name, position, positionId)
+        {
+            this.level = level;
+        }
+
+        public new void GetDetails()
+        {
+            Console.WriteLine("Employee details:\n");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Position: {position} level-{level}");
+            Console.WriteLine($"Employee ID: {positionId}{id}");
+            Console.WriteLine("\n");
+        }
+
+        public new void CheckResponsibilies()
+        {
+            Console.WriteLine("Responsibilities:\n");
+            if (level == 1)
+            {
+                Console.WriteLine("Develop software");
+                Console.WriteLine("Report to level 2");
+            }
+            else if (level == 2)
+            {
+                Console.WriteLine("Review code");
+                Console.WriteLine("Report to manager");
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
