25dc3083d3db1fb035c36edc3c5e126e9544b6e5
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-03 23:47:01 +0600

    access check: protected, protected internal, private protected code added

---
+++ Object Oriented Programming/Encapsulation/OtherAccessModifiers/ManagerDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Encapsulation.OtherAccessModifiers
+{
+    internal class ManagerDetails : EmployeeDetails
+    {
+        public void SetManagerDetails(string department, decimal salaryAmount, string roleName)
+        {
+            // access check: protected, protected internal, private protected
+            this.department = department;
+            this.role = roleName;
+            this.salary = salaryAmount;
+            //this.employeeId = "M101"; // private member is not accessible
+        }
+
+        public void DisplayManagerDetails()
+        {
+            Console.WriteLine($"Manager Department: {department}");
+            Console.WriteLine($"Manager Role: {role}");
+            Console.WriteLine("\n\n");
+        }
+    }
+}
