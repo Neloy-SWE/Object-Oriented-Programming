26dfaa49739b7db2942535c1dc816b5ebeb50cd9
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-04 21:43:51 +0600

    american class added
    class bengal and american are inherit cat class as a breed of cat. hierarchical inheritance applied here for hybrid inheritance

---
+++ Object Oriented Programming/Inheritance/Hybrid/American.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.Hybrid
+{
+    internal class American : Cat
+    {
+        public void TrainingStatus()
+        {
+            Console.WriteLine("Training status: Trained");
+            Console.WriteLine("\n\n");
+        }
+    }
+}
