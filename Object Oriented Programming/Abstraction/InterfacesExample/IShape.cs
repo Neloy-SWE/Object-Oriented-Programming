ff6e99da79b34d5a453929b5491bf0d614cef744
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-05 02:03:01 +0600

    interface: ishape added

---
+++ Object Oriented Programming/Abstraction/InterfacesExample/IShape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Abstraction.InterfacesExample
+{
+    internal interface IShape
+    {
+        void Draw();
+    }
+}
