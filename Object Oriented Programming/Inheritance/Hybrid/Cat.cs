00887657e94ce7dcda7e1c0c6f827cdfd12730d3
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-04 21:31:12 +0600

    cat class added
    in this class we implement single inheritance for hybrid inheritance

---
+++ Object Oriented Programming/Inheritance/Hybrid/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.Hybrid
+{
+    internal class Cat : Animal
+    {
+        public void SetDetails(string breed, int age)
+        {
+            this.breed = breed;
+            this.age = age;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Breed: " + this.breed);
+            Console.WriteLine("Age: " + this.age + " Year");
+        }
+    }
+}
