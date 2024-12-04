4af091f8c4ce58257b25ca9930297149730b275e
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-04 21:22:54 +0600

    hybrid inheritance animal class added

---
+++ Object Oriented Programming/Inheritance/Hybrid/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.Hybrid
+{
+    internal class Animal
+    {
+        internal string breed = "";
+        internal int age = 0;
+    }
+}
