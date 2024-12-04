a947c145b929359ea977de1c2910364e2dab6c58
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-04 21:40:49 +0600

    bengal class added

---
+++ Object Oriented Programming/Inheritance/Hybrid/Bengal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.Hybrid
+{
+    internal class Bengal : Cat
+    {
+        public void GetColor()
+        {
+            Console.WriteLine("Color: Black-White");
+            Console.WriteLine("\n\n");
+        }
+    }
+}
