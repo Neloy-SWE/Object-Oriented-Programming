9b9bcb563414437d4262d02f6654e4dc3bf7edd0
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-05 01:30:01 +0600

    abstract class: shape added

---
+++ Object Oriented Programming/Abstraction/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Abstraction
+{
+    /*
+     * internal class Shape:
+     * is not a abstract class.
+     * so if class is not abstract, 
+     * abstract method cannot be created
+     */
+
+    internal abstract class Shape
+    {
+        public abstract void Draw(); // asbstract method
+        public void Info()
+        {
+            Console.WriteLine("This is general shape class"); // concrete method
+        }
+    }
+}
