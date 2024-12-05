bfb9507ccb233a3ce3c852013b7aa3f8c10b02b6
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-06 00:17:17 +0600

    multiple inheritance example code: error class added

---
+++ Object Oriented Programming/Inheritance/Multiple/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.Multiple
+{
+    internal interface Error
+    {
+        internal void SetMessage(string message);
+    }
+}
