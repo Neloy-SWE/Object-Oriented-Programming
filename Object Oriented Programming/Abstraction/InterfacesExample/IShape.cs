7332bf13239806a08ec544a787bdc49e4f03dc71
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-06 00:21:51 +0600

    ishape class note update

--- Object Oriented Programming/Abstraction/InterfacesExample/IShape.cs
+++ Object Oriented Programming/Abstraction/InterfacesExample/IShape.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * interfaces cannot contain instance fields
+ * interfaces can contain static fields
+ */
+
 namespace Object_Oriented_Programming.Abstraction.InterfacesExample
 {
     internal interface IShape
     {
+        //static string message = ""; // allow
+        //string message; // not allow
         //public abstract void Draw();
         void Draw();
         //{
