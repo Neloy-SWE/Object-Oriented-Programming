7a884b476df912cd3768e16fc439080d84db6280
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-06 00:26:44 +0600

    success class added

---
+++ Object Oriented Programming/Inheritance/Multiple/Success.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.Multiple
+{
+    internal interface Success
+    {
+        internal void SetMessage(string message);
+        internal void Show();
+    }
+}
