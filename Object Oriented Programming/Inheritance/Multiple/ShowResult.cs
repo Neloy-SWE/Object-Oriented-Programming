b79238c5212f097aa654e032350e25cb84ab6f27
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-06 00:26:56 +0600

    show result class added

---
+++ Object Oriented Programming/Inheritance/Multiple/ShowResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming.Inheritance.Multiple
+{
+    internal class ShowResult : Error, Success
+    {
+        private string message = "";
+        public void SetMessage(string message)
+        {
+            this.message = message;
+        }
+
+        public void Show()
+        {
+            Console.Write($"Status: {this.message}");
+        }
+    }
+}
